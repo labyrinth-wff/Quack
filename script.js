76ddc4197b202c98fdbaec24f8db36f66c945097
Labyrinth <126908781+labyrinth-wff@users.noreply.github.com>
2024-09-01 05:21:48 +0300

    Update

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 var audio = new Audio('Quack.mp3');
+audio.load();
+
+var Clicks = 0;
+
+if (localStorage.getItem("Clicks") === null)
+{
+	console.log("Empty");
+	localStorage.setItem("Clicks", Clicks.toString());
+}
+else
+{
+	Clicks = localStorage.getItem("Clicks");
+}
+
+function UpdateClicks()
+{
+	localStorage.setItem("Clicks", Clicks.toString());
+	document.getElementById("Clicks").innerHTML = Clicks.toString().concat(" Clicks!");
+}
+
+//if (document.cookie === "")
+//{
+//	console.log("Empty Cookie");
+//	document.cookie = "C = 0; expires=Tue, 01 Jan 2999 00:00:00 UTC; Secure";
+//}
